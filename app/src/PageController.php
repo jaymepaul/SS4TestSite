c2553abd2937f993db2edc4118bbe1e7893659cf
pjayme <pjayme@silverstripe.com>
2018-10-08 12:41:29 +1300

    Setting up requirements for modules

--- app/src/PageController.php
+++ app/src/PageController.php
@@ -1,8 +1,16 @@
 <?php
 
+use SilverStripe\View\Requirements;
 use CWP\CWP\PageTypes\BasePageController;
 
 class PageController extends BasePageController
 {
-    
+    protected function init()
+    {
+        parent::init();
+
+        Requirements::javascript('nzta/promo-overlay:javascript/promooverlay.js');
+        Requirements::javascript('nzta/promo-overlay:javascript/overlayvideo.js');
+        Requirements::css('nzta/promo-overlay:css/promooverlay.css');
+    }
 }
